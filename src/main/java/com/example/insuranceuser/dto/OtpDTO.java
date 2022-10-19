003bb2005aa26a1c50a3d47cd44bb8d0735879dd
Snehapal2012 <snehapal2012@gamil.com>
2022-10-19 16:28:46 +0530

    Added: Insurance application using spring boot, applied jwt, jms

---
+++ src/main/java/com/example/insuranceuser/dto/OtpDTO.java
@@ -0,0 +1,11 @@
+package com.example.insuranceuser.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class OtpDTO {
+    private String email;
+    private long otp;
+}
